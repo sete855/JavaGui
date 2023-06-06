5ea40827479b84dd8eb018c28c8bd2f98b24804c
ulia <sete855@gmail.com>
2023-06-06 17:49:49 +0300

    TextArea

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,52 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+
+public class Main extends JDialog {
     public static void main(String[] args) {
-        showWelcomeDialog();
-    }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Создаем окно
+                JFrame frame = new JFrame("Запись строк");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(300, 200);
 
-    private static void showWelcomeDialog() {
-        int option = JOptionPane.showConfirmDialog(null, "Добро пожаловать! Хотите зарегистрироваться в программе?",
-                "Регистрация", JOptionPane.YES_NO_OPTION);
-        if (option == JOptionPane.YES_OPTION) {
-            showLoginDialog();
-        } else {
-            System.exit(0);
-        }
-    }
+                // Создаем JTextArea и добавляем в JScrollPane
+                JTextArea textArea = new JTextArea(10, 20);
+                JScrollPane scrollPane = new JScrollPane(textArea);
 
-    private static void showLoginDialog() {
-        String login = "";
-        while (login.length() <= 5 || login.contains(" ")) {
-            login = JOptionPane.showInputDialog(null, "Введите логин (длина должна быть больше 5 символов и не должен содержать пробелов):",
-                    "Логин", JOptionPane.PLAIN_MESSAGE);
-            if (login == null) {
-                System.exit(0);
+                // Создаем JTextField для ввода строки
+                JTextField textField = new JTextField(20);
+
+                // Создаем кнопку "Записать"
+                JButton writeButton = new JButton("Записать");
+
+                // Обработчик события нажатия на кнопку "Записать"
+                writeButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        String text = textField.getText();
+                        if (!text.isEmpty()) {
+                            textArea.append(text + "\n");
+                            textField.setText("");
+                        }
+                    }
+                });
+
+                // Создаем панель для размещения элементов
+                JPanel panel = new JPanel();
+                panel.add(textField);
+                panel.add(writeButton);
+                panel.add(scrollPane);
+
+                // Добавляем панель в окно
+                frame.getContentPane().add(panel);
+
+                // Отображаем окно
+                frame.setLocationRelativeTo(null); // Размещаем окно по центру экрана
+                frame.setVisible(true);
             }
-        }
-        showPasswordDialog();
-    }
-
-    private static void showPasswordDialog() {
-        String password = "";
-        while (!isValidPassword(password)) {
-            JPasswordField passwordField = new JPasswordField();
-            Object[] message = {"Введите пароль (длина должна быть больше 8 символов, не должен содержать пробелов, " +
-                    "должен содержать хотя бы одну цифру и хотя бы одну букву):", passwordField};
-            int option = JOptionPane.showConfirmDialog(null, message, "Пароль", JOptionPane.OK_CANCEL_OPTION);
-            if (option == JOptionPane.CANCEL_OPTION) {
-                System.exit(0);
-            }
-            password = String.valueOf(passwordField.getPassword());
-        }
-        showConfirmPasswordDialog(password);
-    }
-
-    private static void showConfirmPasswordDialog(String password) {
-        String confirmPassword = "";
-        while (!password.equals(confirmPassword)) {
-            JPasswordField passwordField = new JPasswordField();
-            Object[] message = {"Повторите пароль:", passwordField};
-            int option = JOptionPane.showConfirmDialog(null, message, "Повтор пароля", JOptionPane.OK_CANCEL_OPTION);
-            if (option == JOptionPane.CANCEL_OPTION) {
-                System.exit(0);
-            }
-            confirmPassword = String.valueOf(passwordField.getPassword());
-        }
-        showRegistrationSuccessDialog();
-    }
-
-    private static void showRegistrationSuccessDialog() {
-        JOptionPane.showMessageDialog(null, "Регистрация успешно завершена!", "Успешная регистрация",
-                JOptionPane.INFORMATION_MESSAGE);
-        System.exit(0);
-    }
-
-    private static boolean isValidPassword(String password) {
-        if (password.length() < 8 || password.contains(" ")) {
-            return false;
-        }
-        boolean hasLetter = false;
-        boolean hasDigit = false;
-        for (char ch : password.toCharArray()) {
-            if (Character.isLetter(ch)) {
-                hasLetter = true;
-            } else if (Character.isDigit(ch)) {
-                hasDigit = true;
-            }
-            if (hasLetter && hasDigit) {
-                return true;
-            }
-        }
-        return false;
+        });
     }
 }
