38343d8840a8af5a62b40be16cf45442336e15b1
ulia <sete855@gmail.com>
2023-06-06 17:40:46 +0300

    Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,136 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class Main extends JDialog {
+public class Main {
+    private JFrame frame;
+    private JTextField textField;
+    private JButton[] numberButtons;
+    private JButton[] operatorButtons;
+    private JButton equalsButton;
+    private JButton clearButton;
+    private JButton decimalButton;
+    private double firstNumber;
+    private double secondNumber;
+    private double result;
+    private String operator;
+
+    public Main() {
+        frame = new JFrame("Калькулятор");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(30, 30, 240, 30);
+        frame.add(textField);
+
+        numberButtons = new JButton[10];
+        operatorButtons = new JButton[4];
+
+        for (int i = 0; i < numberButtons.length; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+        }
+
+        operatorButtons[0] = new JButton("+");
+        operatorButtons[1] = new JButton("-");
+        operatorButtons[2] = new JButton("*");
+        operatorButtons[3] = new JButton("/");
+
+        equalsButton = new JButton("=");
+        clearButton = new JButton("С");
+        decimalButton = new JButton(".");
+
+        numberButtons[0].setBounds(30, 90, 50, 50);
+        frame.add(numberButtons[0]);
+
+        decimalButton.setBounds(90, 90, 50, 50);
+        frame.add(decimalButton);
+
+        clearButton.setBounds(150, 90, 50, 50);
+        frame.add(clearButton);
+
+        for (int i = 1; i < numberButtons.length; i++) {
+            int row = (i - 1) / 3;
+            int col = (i - 1) % 3;
+            numberButtons[i].setBounds(30 + col * 60, 150 + row * 60, 50, 50);
+            frame.add(numberButtons[i]);
+        }
+
+        for (int i = 0; i < operatorButtons.length; i++) {
+            operatorButtons[i].setBounds(210, 150 + i * 60, 50, 50);
+            frame.add(operatorButtons[i]);
+        }
+
+        equalsButton.setBounds(90, 330, 130, 50);
+        frame.add(equalsButton);
+
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+        for (int i = 0; i < numberButtons.length; i++) {
+            int number = i;
+            numberButtons[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    textField.setText(textField.getText() + number);
+                }
+            });
+        }
+
+        decimalButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!textField.getText().contains(".")) {
+                    textField.setText(textField.getText() + ".");
+                }
+            }
+        });
+
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("");
+            }
+        });
+
+        for (int i = 0; i < operatorButtons.length; i++) {
+            int index = i;
+            operatorButtons[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    firstNumber = Double.parseDouble(textField.getText());
+                    operator = operatorButtons[index].getText();
+                    textField.setText("");
+                }
+            });
+        }
+
+        equalsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                secondNumber = Double.parseDouble(textField.getText());
+                switch (operator) {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber == 0) {
+                            result = 0; // Здесь можно добавить обработку деления на 0
+                        } else {
+                            result = firstNumber / secondNumber;
+                        }
+                        break;
+                }
+                textField.setText(String.valueOf(result));
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
             public void run() {
-                // Создаем окно
-                JFrame frame = new JFrame("Кликер");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(300, 200);
-
-                // Создаем кнопку
-                JButton button = new JButton("Нажми меня!");
-
-                // Создаем надпись
-                JLabel label = new JLabel("Количество нажатий: 0");
-
-                // Обработчик события нажатия на кнопку
-                button.addActionListener(new ActionListener() {
-                    int count = 0;
-
-                    public void actionPerformed(ActionEvent e) {
-                        count++;
-                        label.setText("Количество нажатий: " + count);
-                    }
-                });
-
-                // Создаем панель для размещения кнопки и надписи
-                JPanel panel = new JPanel();
-                panel.add(button);
-                panel.add(label);
-
-                // Добавляем панель в окно
-                frame.getContentPane().add(panel);
-
-                // Отображаем окно
-                frame.setLocationRelativeTo(null); // Размещаем окно по центру экрана
-                frame.setVisible(true);
+                new Main();
             }
         });
     }
