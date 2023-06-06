1cb743374d714c24f6076a6d354b88d4b887af51
ulia <sete855@gmail.com>
2023-06-06 17:41:45 +0300

    Изменение шрифта надписи

--- src/Main.java
+++ src/Main.java
@@ -1,138 +1,46 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-
-public class Main {
-    private JFrame frame;
-    private JTextField textField;
-    private JButton[] numberButtons;
-    private JButton[] operatorButtons;
-    private JButton equalsButton;
-    private JButton clearButton;
-    private JButton decimalButton;
-    private double firstNumber;
-    private double secondNumber;
-    private double result;
-    private String operator;
-
-    public Main() {
-        frame = new JFrame("Калькулятор");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 400);
-        frame.setLayout(null);
-
-        textField = new JTextField();
-        textField.setBounds(30, 30, 240, 30);
-        frame.add(textField);
-
-        numberButtons = new JButton[10];
-        operatorButtons = new JButton[4];
-
-        for (int i = 0; i < numberButtons.length; i++) {
-            numberButtons[i] = new JButton(String.valueOf(i));
-        }
-
-        operatorButtons[0] = new JButton("+");
-        operatorButtons[1] = new JButton("-");
-        operatorButtons[2] = new JButton("*");
-        operatorButtons[3] = new JButton("/");
-
-        equalsButton = new JButton("=");
-        clearButton = new JButton("С");
-        decimalButton = new JButton(".");
-
-        numberButtons[0].setBounds(30, 90, 50, 50);
-        frame.add(numberButtons[0]);
-
-        decimalButton.setBounds(90, 90, 50, 50);
-        frame.add(decimalButton);
-
-        clearButton.setBounds(150, 90, 50, 50);
-        frame.add(clearButton);
-
-        for (int i = 1; i < numberButtons.length; i++) {
-            int row = (i - 1) / 3;
-            int col = (i - 1) % 3;
-            numberButtons[i].setBounds(30 + col * 60, 150 + row * 60, 50, 50);
-            frame.add(numberButtons[i]);
-        }
-
-        for (int i = 0; i < operatorButtons.length; i++) {
-            operatorButtons[i].setBounds(210, 150 + i * 60, 50, 50);
-            frame.add(operatorButtons[i]);
-        }
-
-        equalsButton.setBounds(90, 330, 130, 50);
-        frame.add(equalsButton);
-
-        frame.setVisible(true);
-        frame.setResizable(false);
-
-        for (int i = 0; i < numberButtons.length; i++) {
-            int number = i;
-            numberButtons[i].addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    textField.setText(textField.getText() + number);
-                }
-            });
-        }
-
-        decimalButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (!textField.getText().contains(".")) {
-                    textField.setText(textField.getText() + ".");
-                }
-            }
-        });
-
-        clearButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
-            }
-        });
-
-        for (int i = 0; i < operatorButtons.length; i++) {
-            int index = i;
-            operatorButtons[i].addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textField.getText());
-                    operator = operatorButtons[index].getText();
-                    textField.setText("");
-                }
-            });
-        }
-
-        equalsButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                secondNumber = Double.parseDouble(textField.getText());
-                switch (operator) {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-":
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "*":
-                        result = firstNumber * secondNumber;
-                        break;
-                    case "/":
-                        if (secondNumber == 0) {
-                            result = 0; // Здесь можно добавить обработку деления на 0
-                        } else {
-                            result = firstNumber / secondNumber;
-                        }
-                        break;
-                }
-                textField.setText(String.valueOf(result));
-            }
-        });
-    }
-
+public class Main extends JDialog {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                new Main();
+                // Создаем окно
+                JFrame frame = new JFrame("Пример метки с изменяемым размером шрифта");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(800, 600);
+
+                // Создаем метку
+                JLabel label = new JLabel("Моя первая надпись!");
+                label.setFont(new Font("Arial", Font.ITALIC, 50));
+
+                // Создаем слайдер для изменения размера шрифта
+                JSlider slider = new JSlider(JSlider.HORIZONTAL, 5, 100, 50);
+                slider.setMajorTickSpacing(10);
+                slider.setMinorTickSpacing(5);
+                slider.setPaintTicks(true);
+                slider.setPaintLabels(true);
+                slider.addChangeListener(new ChangeListener() {
+                    public void stateChanged(ChangeEvent e) {
+                        int fontSize = slider.getValue();
+                        label.setFont(label.getFont().deriveFont((float) fontSize));
+                    }
+                });
+
+                // Создаем панель для размещения метки и слайдера
+                JPanel panel = new JPanel(new BorderLayout());
+                panel.add(label, BorderLayout.CENTER);
+                panel.add(slider, BorderLayout.SOUTH);
+
+                // Добавляем панель в окно
+                frame.getContentPane().add(panel);
+
+                // Отображаем окно
+                frame.setLocationRelativeTo(null); // Размещаем окно по центру экрана
+                frame.setVisible(true);
             }
         });
     }
